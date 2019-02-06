1141bc3863651e9fc44e54adb977c2e8a9102ebe
Raphael Michel <mail@raphaelmichel.de>
2019-02-06 15:05:02 +0100

    Port over changes from libpretixprint-itext

--- libpretixprint/src/main/java/eu/pretix/libpretixprint/templating/Layout.java
+++ libpretixprint/src/main/java/eu/pretix/libpretixprint/templating/Layout.java
@@ -2,47 +2,52 @@ package eu.pretix.libpretixprint.templating;
 
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.Element;
-import com.lowagie.text.Font;
-import com.lowagie.text.Image;
-import com.lowagie.text.Paragraph;
-import com.lowagie.text.pdf.BaseFont;
-import com.lowagie.text.pdf.ColumnText;
-import com.lowagie.text.pdf.PdfContentByte;
-import com.lowagie.text.pdf.PdfReader;
-import com.lowagie.text.pdf.PdfWriter;
-
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.*;
+import com.lowagie.text.pdf.codec.Base64;
+import eu.pretix.libpretixprint.helpers.BarcodeQR;
+import eu.pretix.libpretixprint.helpers.EmbeddedLogos;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
-
-import eu.pretix.libpretixprint.helpers.BarcodeQR;
 
 import static com.lowagie.text.Utilities.millimetersToPoints;
 
 public class Layout {
     private JSONArray elements;
     private InputStream backgroundInputStream;
-    private ContentProvider contentProvider;
+    private Iterator<ContentProvider> contentProviders;
 
-    public Layout(JSONArray elements, String background, ContentProvider contentProvider) throws FileNotFoundException {
+    public Layout(JSONArray elements, String background, Iterator<ContentProvider> contentProvider) throws FileNotFoundException {
         this(elements, new FileInputStream(background), contentProvider);
     }
 
-    public Layout(JSONArray elements, InputStream background, ContentProvider contentProvider) {
+    public Layout(JSONArray elements, InputStream background, Iterator<ContentProvider> contentProviders) {
         this.elements = elements;
         this.backgroundInputStream = background;
-        this.contentProvider = contentProvider;
+        this.contentProviders = contentProviders;
+    }
+
+    private void drawPoweredBy(JSONObject data, String style, PdfContentByte cb) throws IOException, DocumentException, JSONException {
+        String b64data = "";
+        if (style.equals("white")) {
+            b64data = EmbeddedLogos.POWERED_BY_PRETIX_WHITE;
+        } else {
+            b64data = EmbeddedLogos.POWERED_BY_PRETIX_DARK;
+        }
+        Image img = Image.getInstance(Base64.decode(b64data));
+        float size = millimetersToPoints((float) data.getDouble("size"));
+        img.scalePercent(size * 100f / img.getPlainHeight());
+        cb.addImage(
+                img, img.getScaledWidth(), 0, 0, img.getScaledHeight(),
+                millimetersToPoints((float) data.getDouble("left")),
+                millimetersToPoints((float) data.getDouble("bottom"))
+        );
     }
 
     private void drawQrCode(JSONObject data, String text, PdfContentByte cb) throws IOException, DocumentException, JSONException {
@@ -55,10 +60,11 @@ public class Layout {
                 hints
         );
 
+        float size = millimetersToPoints((float) data.getDouble("size"));
         Image img = bqr.getImage();
-        img.scaleAbsoluteHeight(millimetersToPoints((float) data.getDouble("size")));
+        img.scaleToFit(size, size);
         cb.addImage(
-                img, img.getWidth(), 0, 0, img.getHeight(),
+                img, img.getScaledWidth(), 0, 0, img.getScaledHeight(),
                 millimetersToPoints((float) data.getDouble("left")),
                 millimetersToPoints((float) data.getDouble("bottom"))
         );
@@ -78,6 +84,10 @@ public class Layout {
 
         FontRegistry fontRegistry = FontRegistry.getInstance();
         BaseFont baseFont = fontRegistry.get(data.getString("fontfamily"), style);
+        if (baseFont == null) {
+            System.out.print("Unable to load font " + data.getString("fontfamily"));
+            baseFont = fontRegistry.get("Open Sans", style);
+        }
         Font font = new Font(baseFont, fontsize);
 
         font.setColor(
@@ -130,28 +140,52 @@ public class Layout {
         ct.go();
     }
 
-    public void render(String outputFilename)
+    public void render(String filename)
             throws Exception {
-        PdfReader reader = new PdfReader(backgroundInputStream);
-        if (reader.getNumberOfPages() < 1) {
-            throw new Exception("Background PDF does not have a first page.");
-        }
+        render(new FileOutputStream(filename));
+    }
 
-        Document document = new Document(reader.getPageSize(1));
-        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(outputFilename));
+    public void render(OutputStream os)
+            throws Exception {
+        Document document;
+        PdfReader reader = null;
+        if (backgroundInputStream != null) {
+            reader = new PdfReader(backgroundInputStream);
+            if (reader.getNumberOfPages() < 1) {
+                throw new Exception("Background PDF does not have a first page.");
+            }
+
+            document = new Document(reader.getPageSize(1));
+        } else {
+            document = new Document(new RectangleReadOnly(
+                    (float) 8 * 72,
+                    (float) (3.25 * 72)
+            ));
+        }
+        PdfWriter writer = PdfWriter.getInstance(document, os);
         document.open();
 
         PdfContentByte cb = writer.getDirectContent();
 
-        // Copy background
-        cb.addTemplate(writer.getImportedPage(reader, 1), 0, 0);
+        while (contentProviders.hasNext()) {
+            ContentProvider cp = contentProviders.next();
 
-        for (int i = 0; i < elements.length(); i++) {
-            JSONObject obj = elements.getJSONObject(i);
-            if (obj.getString("type").equals("barcodearea")) {
-                drawQrCode(obj, contentProvider.getBarcodeContent(obj.optString("content")), cb);
-            } else if (obj.getString("type").equals("textarea")) {
-                drawTextarea(obj, contentProvider.getTextContent(obj.getString("content"), obj.getString("text")), cb);
+            if (reader != null) {
+                cb.addTemplate(writer.getImportedPage(reader, 1), 0, 0);
+            }
+
+            for (int i = 0; i < elements.length(); i++) {
+                JSONObject obj = elements.getJSONObject(i);
+                if (obj.getString("type").equals("barcodearea")) {
+                    drawQrCode(obj, cp.getBarcodeContent(obj.optString("content")), cb);
+                } else if (obj.getString("type").equals("textarea")) {
+                    drawTextarea(obj, cp.getTextContent(obj.getString("content"), obj.getString("text")), cb);
+                } else if (obj.getString("type").equals("poweredby")) {
+                    drawPoweredBy(obj, obj.getString("content"), cb);
+                }
+            }
+            if (contentProviders.hasNext()) {
+                document.newPage();
             }
         }
         document.close();
