afa25816ae7424bea469620d9cab5310df215a16
Raphael Michel <michel@rami.io>
2023-05-23 16:33:32 +0200

    Fix empty QR codes being changed to spaces

--- libpretixprint/src/main/java/eu/pretix/libpretixprint/templating/Layout.java
+++ libpretixprint/src/main/java/eu/pretix/libpretixprint/templating/Layout.java
@@ -321,9 +321,6 @@ public class Layout {
                     }
                     if (obj.getString("type").equals("barcodearea")) {
                         String content = cp.getBarcodeContent(obj.optString("content"), obj.optString("text", ""), obj.optJSONObject("text_i18n"));
-                        if (content.isEmpty()) {
-                            content = " ";  // ZXing crashes when asked to draw empty QR codes
-                        }
                         drawQrCode(obj, content, obj.optBoolean("nowhitespace", false), cb);
                     } else if (obj.getString("type").equals("textarea")) {
                         drawTextarea(obj, cp.getTextContent(obj.getString("content"), obj.optString("text", ""), obj.optJSONObject("text_i18n")), cb);
